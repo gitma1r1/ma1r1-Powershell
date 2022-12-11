10b268eacd800cb3066345b39f7a957b27f7e239
ma1r1 <58610737+gitma1r1@users.noreply.github.com>
2022-12-11 15:13:04 +0100

    Add files via upload

---
+++ Monitor_Windows_File_Change.ps1
@@ -0,0 +1,32 @@
+﻿$log = "$home\Desktop\Log.txt"
+$pathtomonitor = "D:\temp\test123"
+$timeout = 1000
+
+try {
+$FileSystemWatcher = New-Object System.IO.FileSystemWatcher $pathtomonitor
+$FileSystemWatcher.IncludeSubdirectories = $true
+
+Write-Host "Monitoring content of $PathToMonitor"
+while ($true) {
+  $change = $FileSystemWatcher.WaitForChanged('All', $timeout)
+  if ($change.TimedOut -eq $false)
+  {
+      # get information about the changes detected
+      Write-Host "Change detected:"
+      # invoke some actions here when change detected
+      if (!($change.ChangeType -eq 'Deleted')){
+          #Start-Process $change.name -WorkingDirectory $pathtomonitor -verb PrintTo '\\printerserver\printer1' -PassThru | %{ sleep 10;$_ } | kill
+      }
+      $change | Out-Default
+      (Get-Date).ToString() + ", " + $change.ChangeType.ToString() + ", " + $change.Name | Out-File $log -Append
+   }
+  else
+  {
+      Write-Host "." -NoNewline
+  }
+ }
+}
+finally{
+  $FileSystemWatcher.Dispose()
+  Write-Host "My Watcher is done."
+}
