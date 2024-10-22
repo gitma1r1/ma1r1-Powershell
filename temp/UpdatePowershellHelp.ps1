3f05675440c8cfbab2dd5f5ac45ad123aaffd76e
ma1r1 <mairinger@msn.com>
2024-10-22 22:50:40 +0200

    test

---
+++ temp/UpdatePowershellHelp.ps1
@@ -0,0 +1,24 @@
+#requires -module ScheduledTasks
+
+#update PowerShell help via a Scheduled Task
+$cred = Get-Credential "$env:USERDOMAIN\$env:USERNAME"
+
+# for Windows PowerShell
+# $action = New-ScheduledTaskAction -execute  powershell.exe -argument '-noprofile -command "&{Update-Help -force}"'
+$actionparams = @{
+    execute  = 'pwsh.exe'
+    argument = '-nologo -noprofile -noninteractive -command "&{Update-Help -force}"'
+}
+$action = New-ScheduledTaskAction @actionparams
+
+$params = @{
+    TaskName = "Monthly PowerShell Core Help Update"
+    TaskPath = "\Microsoft\Windows\PowerShell"
+    Trigger  = New-ScheduledTaskTrigger -At 8:00AM -Weekly -WeeksInterval 4 -DaysOfWeek Monday
+    Action   = $action
+    User     = $cred.UserName
+    Password = $cred.GetNetworkCredential().Password
+    Runlevel = "Highest"
+}
+
+Register-ScheduledTask @params
